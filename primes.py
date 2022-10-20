5da579d78af1b840c0f84b716b6cd5a2696dfb35
Chuliang Li <chuliang.li@kcl.ac.uk>
2022-10-20 21:26:56 +0100

    This is the complete code of finding the first n numbers of prime numbers

--- primes.py
+++ primes.py
@@ -1,6 +1,26 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def primes(number_of_primes):
+def isPrime(number):
+    if number == 2:
+        return True
+    else:
+        for i in range(2, number//2 + 1):
+            if number % i == 0:
+                return False
+        return True
+
+def primes(n):
+    i = 2
     list = []
-    return list
+    if n <= 0:
+        raise ValueError
+    else:
+        while True:
+            if isPrime(i):
+                list.append(i)
+                if len(list) == n:
+                    break
+            i = i + 1
+
+    return(list)
